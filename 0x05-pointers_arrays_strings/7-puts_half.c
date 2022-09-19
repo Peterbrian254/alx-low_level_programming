a8b4904c0a7651c97524ff7ec8097e6e8ccdca30
peterbrian254 <peterbrian8957@gmail.com>
2022-09-19 10:00:39 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,7 +2,7 @@
 
 /**
  * puts_half - a function that prints half of a string
- * if odd len, n = (length_of_the_string - 1) / 2
+ *
  * @str: input
  * Return: half of input
  */
@@ -33,4 +33,3 @@ void puts_half(char *str)
 		_putchar('\n');
 	}
 }
-
