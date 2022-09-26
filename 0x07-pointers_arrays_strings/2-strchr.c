4b3c4079ef7f0ba18a9d36b01e8ba44162ecaecd
peterbrian254 <peterbrian8957@gmail.com>
2022-09-26 10:45:22 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,23 +8,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	int size_l = 0, i = 0;
-	char *p;
-	while (s[i])
-		i++;
-	if (i > 0)
+	int i;
+
+	for (i = 0; *(s + i); i++)
 	{
-		for (size_l = 0; size_l <= i;)
-		{
-			if (c == s[size_l])
-			{
-				p = s + size_l;
-				break;
-			}
-			p = 0;
-			size_l++;
-		}
-	} else
-		p = 0;
-	return (p);
+		if (*(s + i) == c)
+			return (s + i);
+	}
+	if (*(s + i) == c)
+		return (s + i);
+	return (0);
 }
